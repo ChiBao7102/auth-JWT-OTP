30718faebf72c5f3e47e8849d1f1eccbb259a564
Cao Chi Bao <baohot1975@hotmail.com>
2023-11-29 15:14:13 +0700

    Authtication_Admin

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -1,38 +1,36 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 
 class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+        $this->middleware('auth:admin', ['except' => ['login', 'register']]);
     }
 
     public function login(Request $request)
     {
-        // dd($request->all());
         $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
         $credentials = $request->only('email', 'password');
-        $token = Auth::attempt($credentials);
-        // dd($token);
+        $token = Auth::guard('admin')->attempt($credentials);
         if (!$token) {
             return response()->json([
                 'message' => 'Unauthorized',
             ], 401);
         }
 
-        $user = Auth::user();
-
+        $user = Auth::guard('admin')->user();
+        // dd($user);
         return response()->json([
             'user' => $user,
             'authorization' => [
@@ -45,37 +43,17 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
+        $user = Admin::create([
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
         return response()->json([
             'message' => 'User created successfully',
-            'user' => $user
-        ]);
-    }
-
-    public function logout()
-    {
-        Auth::logout();
-        return response()->json([
-            'message' => 'Successfully logged out',
-        ]);
-    }
-
-    public function getInfo()
-    {
-        dd(Auth::check());
-        $user = Auth::user();
-        return response()->json([
-            'message' => 'Successfully logged info',
             'user' => $user
         ]);
     }

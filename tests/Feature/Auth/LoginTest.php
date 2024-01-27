b9464b92c3f2a0db4713e1ad5865b2e5529a669e
Cao Chi Bao <baohot1975@hotmail.com>
2024-01-27 09:48:34 +0700

    Setting and check CI with phpunit

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    public function testRequireEmailAndLogin()
+    {
+        $this->json('POST', 'v1/api/login')
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'email' => ['The email field is required.'],
+                    'password' => ['The password field is required.']
+                ]
+            ]);
+
+    }
+}
